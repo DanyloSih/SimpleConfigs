510bedec30cce9a288ec314afd35d4e35dc92571
DanyloSih <geratosfen@gmail.com>
2024-03-11 23:43:05 +0200

    Changed folders hierarhy

---
+++ SimpleConfigs.Test/Configs/TestConfigWithoutAnyAttributesAndInterfaces.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleConfigs.Test.Configs
+{
+    public class TestConfigWithoutAnyAttributesAndInterfaces
+    {
+        public string CharacterName = "TestCharacterName";
+        public int ChraracterMaxHP = 100;
+    }
+}
