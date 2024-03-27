23e22f045a89c5cc145dc0a55d9b5c414b6fdd26
DanyloSih <geratosfen@gmail.com>
2024-03-27 13:55:24 +0200

    Update NuGet packages.

---
+++ SimpleConfigs.JSON/Core/LocalJSONConfigsService.cs
@@ -0,0 +1,18 @@
+﻿using SimpleConfigs.Core;
+using SimpleConfigs.JSON.SerializationManagers;
+
+namespace SimpleConfigs.JSON.Core
+{
+    public class LocalJSONConfigsService : ConfigsService
+    {
+        public LocalJSONConfigsService(params Type[] registeringConfigTypes) 
+            : base(new JsonSerializationManager(), new LocalFileSystem(), registeringConfigTypes)
+        {
+        }
+
+        public LocalJSONConfigsService(params (Type, PathSettings?)[] registeringConfigTypes) 
+            : base(new JsonSerializationManager(), new LocalFileSystem(), registeringConfigTypes)
+        {
+        }
+    }
+}
